c5006d0490d46f33b87f763a4a75e70baba2a25b
pailecheng <981005580@qq.com>
2023-03-17 17:06:23 +0800

    Update index.vue

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -75,6 +75,10 @@ watch(
 				<div class="flex-1 min-h-0 pb-4 overflow-hidden">
           <List />
         </div>
+				<div class="p-4">
+          <NButton block @click="show = true">
+          </NButton>
+        </div>
       </main>
     </div>
   </NLayoutSider>
